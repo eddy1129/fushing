fef566e99445b1c91d5c961e024c23f65750cc41
eddychan1129 <yuenho12345@gmail.com>
2023-05-18 15:44:39 +0800

    Search name english

--- src/component/Sildebar.js
+++ src/component/Sildebar.js
@@ -5,7 +5,7 @@ import { Button } from "react-bootstrap";
 import { useState, useEffect, useContext } from "react";
 import { TableContext } from "../store/Table-context";
 import ProductItem from "./ProductItem";
-import productsData from "../Product/ProductData"
+import productsData from "../Product/ProductData";
 
 function Sildebar() {
   let { setTableItems } = useContext(TableContext);
@@ -35,10 +35,28 @@ function Sildebar() {
     setTableItems(init_productsList);
   };
 
-
   const showChristmasProduct = () => {
     const init_productsList = productsData.map((item) =>
-      item.product_type === "Christmas" ? (
+      item.product_name.includes(input) ? (
+        <ProductItem
+          key={item.id}
+          id={item.id}
+          product_type={item.product_type}
+          product_name={item.product_name}
+          size={item.size}
+          price={item.price}
+          image={item.image}
+        />
+      ) : null
+    );
+
+    setTableItems(init_productsList);
+  };
+
+  const searchProduct = () => {
+    console.log(input)
+    const init_productsList = productsData.map((item) =>
+      item.product_name.includes(input) ? (
         <ProductItem
           key={item.id}
           id={item.id}
@@ -66,15 +84,17 @@ function Sildebar() {
           aria-describedby="inputGroup-sizing-sm"
           onChange={(e) => setInput(e.target.value)}
         />
-        <Button style={{ marginLeft: "20px" }}>搜尋產品</Button>
+        <Button style={{ marginLeft: "20px" }} onClick={searchProduct}>
+          搜尋產品
+        </Button>
       </InputGroup>
       <Nav.Link onClick={showNewYearProduct}>新年裝飾</Nav.Link>
       <Nav.Link onClick={showChristmasProduct}>聖誕裝飾</Nav.Link>
-      <Nav.Link onClick={showChristmasProduct}>萬聖裝飾 </Nav.Link>
+      <Nav.Link onClick={showNewYearProduct}>萬聖裝飾 </Nav.Link>
       <Nav.Link onClick={showChristmasProduct}>中秋裝飾</Nav.Link>
-      <Nav.Link onClick={showChristmasProduct}>兒童服飾</Nav.Link>
+      <Nav.Link onClick={showNewYearProduct}>兒童服飾</Nav.Link>
       <Nav.Link onClick={showChristmasProduct}>成年服飾</Nav.Link>
-      <Nav.Link onClick={showChristmasProduct}>玩具</Nav.Link>
+      <Nav.Link onClick={showNewYearProduct}>玩具</Nav.Link>
     </Nav>
   );
 }
