58a046761f97ddfe919bca1a00beca37db5a7a28
eddychan1129 <yuenho12345@gmail.com>
2023-05-19 17:55:15 +0800

    adjust placeholder font size

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,9 +1,9 @@
-import { Button } from "react-bootstrap";
 import { useState, useEffect, useContext } from "react";
 import { TableContext } from "../store/Table-context";
 import ProductItem from "./ProductItem";
 import productsData from "../Product/ProductData";
 import classes from "./navbar.module.css";
+import { SearchOutlined } from "@ant-design/icons";
 
 function Sildebar() {
   let { setTableItems } = useContext(TableContext);
@@ -72,33 +72,43 @@ function Sildebar() {
   return (
     <ul>
       <li>
-        <a href="#1" className={classes.active}>FuShing</a>
+        <a href="#1" className={classes.active}>
+          FuShing
+        </a>
       </li>
       <li>
-        <a href="#2" onClick={showNewYearProduct}>新年裝飾</a>
+        <a href="#2" onClick={showNewYearProduct}>
+          新年裝飾
+        </a>
       </li>
       <li className={classes.dropdown}>
         <a href="#3" onClick={showNewYearProduct} className={classes.dropbtn}>
           萬聖裝飾
         </a>
         <div className={classes.dropdownContent}>
-          <a href="#2" onClick={showChristmasProduct}>聖誕裝飾</a>
+          <a href="#2" onClick={showChristmasProduct}>
+            聖誕裝飾
+          </a>
           <a href="#3">中秋裝飾</a>
           <a href="#4">兒童服飾</a>
           <a href="#5">成年服飾</a>
           <a href="#6">玩具</a>
         </div>
       </li>
-
-      <div className={classes.searchBar}>
-        <div style={{ display: "flex", height: "30px" }}>
-          <input
-            onChange={(e) => setInput(e.target.value)}
-            style={{ width: "100px" }}
-          />
-          <Button onClick={searchProduct}>搜尋產品</Button>
-        </div>
-      </div>
+      <li className={classes.searchBar}>
+      <a href="#6">
+        <input
+          placeholder="搜尋商品"
+          onChange={(e) => setInput(e.target.value)}
+          style={{ width: "100px" }}
+        />
+        <SearchOutlined
+          className={classes.searchBtn}
+          style={{ fontSize: "20px", marginLeft: "5px" }}
+          onClick={searchProduct}
+        />
+      </a>
+      </li>
     </ul>
   );
 }
