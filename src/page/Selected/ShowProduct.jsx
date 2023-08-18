3106a26837fdcb470e45327142de1bb028353a5e
eddy1129 <yuenho12345@gmail.com>
2023-08-18 16:25:54 +0800

    ADDED Pagination

--- src/page/Selected/ShowProduct.jsx
+++ src/page/Selected/ShowProduct.jsx
@@ -1,5 +1,5 @@
 import Sildebar from "../../component/navbar";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TableContext } from "../../store/Table-context";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,12 +9,12 @@ import aData from "../../Product/Adata";
 import Lanterns from "../../Product/Lanterns";
 import GlowStick from "../../Product/GlowStick";
 import ProductItem from "../../component/ProductItem";
+import PaginationBasic from "../../component/PaginationBasic";
 
 export default function ShowProduct() {
   let params = useParams();
   let dataX;
   let { tableItems, setTableItems } = useContext(TableContext);
-
   switch (params.product_type) {
     case "GlowStick":
       dataX = GlowStick;
@@ -29,8 +29,17 @@ export default function ShowProduct() {
       dataX = productsData;
   }
 
+  const [page, setPage] = useState(1);
+  const limit = 8;
+  const pageCount = Math.ceil(dataX.length / limit);
+  const start = 0 + (page - 1) * limit;
+  const currentData = dataX.slice(start, start + limit);
+ 
+
+
+
   useEffect(() => {
-    const productsList = dataX.map((item) => {
+    const productsList = currentData.map((item) => {
       return (
         <ProductItem
           key={item.id}
@@ -44,13 +53,14 @@ export default function ShowProduct() {
       );
     });
     setTableItems(productsList);
-  }, [dataX, setTableItems]);
+  }, [dataX, setTableItems,setPage,page]);
 
   return (
     <Container>
       <Sildebar />
-      <Row xs={1} md={4} className="g-4">
+      <Row xs={1} md={4} className="g-1">
         {tableItems}
+        <PaginationBasic pageCount={pageCount} page={page} setPage={setPage} />
       </Row>
     </Container>
   );
