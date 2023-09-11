2d4cee165ce71a2da5845096a1da63e7a029cc69
YuenHo <yuenho12345@gmail.com>
2023-09-11 08:01:21 +0800

    change width

--- src/component/PaginationBasic.jsx
+++ src/component/PaginationBasic.jsx
@@ -8,23 +8,18 @@ function PaginationBasic(props) {
       <Pagination.Item
         key={number}
         active={number === props.page}
-        onClick={
-          () => {
-            props.setPage(number)
-            window.scrollTo(0, 0);
-          }
-
-        }
+        onClick={() => {
+          props.setPage(number);
+          window.scrollTo(0, 0);
+        }}
       >
         {number}
-
       </Pagination.Item>
     );
   }
 
   return (
-    <div style={{marginTop:"30px"}}>
-      
+    <div style={{ marginTop: "30px", width: "100vw" }}>
       <Pagination size="lg">{items}</Pagination>
     </div>
   );
